b6b4778e59be89d48347de066a7dc241c6c07713
Lindsey  Lasswell <>
2021-06-04 01:53:05 +0000

    Update to include solutions to Part 1.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,9 +3,9 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = " ";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
+let question = "Who was the first woman in space? ";
 let correctAnswer;
 let candidateAnswer;
 let questions;
@@ -15,11 +15,17 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName = input.question("What is the candidate name? " );
+  console.log("Welcome " + candidateName + "!");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+  candidateAnswer = input.question(question);
+  console.log(candidateAnswer)
+  correctAnswer = "Sally Ride"
+
+    console.log(candidateAnswer)
 
 
 }
@@ -27,7 +33,11 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+    if (candidateAnswer === "Sally Ride" ) {
+      console.log("Woo hoo! You are correct!");
+    } else {
+      console.log("Not quite! Try again.")
+    }
 
   let grade;
   
